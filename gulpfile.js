b1ca528e5f1d683f4d949dbbd48222a16189f5d9
Jens Melgaard <jmd@local.dotjem.com>
2016-06-02 22:25:40 +0200

    simple gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
 var gulp = require("gulp");
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 var ts = require("gulp-typescript");
 var tsProject = ts.createProject("tsconfig.json");
 
 gulp.task("default", function () {
     return tsProject.src()
         .pipe(ts(tsProject))
-        .js.pipe(gulp.dest("dist"));
+        .js
+        .pipe(rename('dotjem-angular-events.js'))
+        .pipe(gulp.dest("dist"))
+        .pipe(rename('dotjem-angular-events.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
 });
